2bd4631084c4cea0648bc15a1cdd673510d4b8a9
Joziudigital <jorge.zapatas@est.iudigital.edu.co>
2025-12-15 08:18:21 -0500

    act

---
+++ main.py
@@ -0,0 +1,18 @@
+from miniturtle import adelante, abajo, reiniciar
+
+#comandos de uso
+
+reiniciar()
+adelante(100)
+abajo(50)
+
+reiniciar()
+
+adelante(50)
+abajo(100)
+
+reiniciar()
+
+adelante(150)
+abajo(75)
+
