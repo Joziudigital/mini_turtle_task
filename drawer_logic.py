f01a5259c1edaea820aca70f40a7672acfad0dad
Joziudigital <jorge.zapatas@est.iudigital.edu.co>
2025-12-15 08:54:43 -0500

    act

---
+++ drawer_logic.py
@@ -0,0 +1,25 @@
+position_x= 0
+position_y= 0
+
+def adelante(pasos):
+    global position_x, position_y
+    position_x += pasos
+    print(" "* position_x + "-" * pasos + "m>")
+    
+    
+def abajo(pasos):
+    global position_x, position_y
+    
+    for i in range(pasos):
+        print(" " * position_x + "|")
+        
+    print(" " * position_x + "3")
+    print(" " * position_x + "V")
+    position_y += pasos
+    
+def reiniciar():
+    global position_x, position_y
+    position_x= 0
+    position_y= 0
+    
+    
