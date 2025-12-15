038f36f182c8ed7565a82d3e50bfd09d64ce0ca2
Joziudigital <jorge.zapatas@est.iudigital.edu.co>
2025-12-15 08:33:45 -0500

    act

--- mini_turtle/drawer_logic.py
+++ mini_turtle/drawer_logic.py
@@ -12,3 +12,14 @@ def abajo(pasos):
     
     for i in range(pasos):
         print(" " * position_x + "|")
+        
+    print(" " * position_x + "3")
+    print(" " * position_x + "V")
+    position_y += pasos
+    
+def reiniciar():
+    global position_x, position_y
+    position_x= 0
+    position_y= 0
+    
+    
