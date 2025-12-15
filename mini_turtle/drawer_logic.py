291672453e4f767c74563e2e0b6380dcaac3d853
Joziudigital <jorge.zapatas@est.iudigital.edu.co>
2025-12-15 08:31:05 -0500

    act

---
+++ mini_turtle/drawer_logic.py
@@ -0,0 +1,14 @@
+position_x= 0
+position_y= 0
+
+def adelante(pasos):
+    global position_x, position_y
+    position_x += pasos
+    print(position_x + "-" * pasos + "m>")
+    
+    
+def abajo(pasos):
+    global position_x, position_y
+    
+    for i in range(pasos):
+        print(" " * position_x + "|")
